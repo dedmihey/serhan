b2c9f6fee8699707aaf06b653de9a786f52344b7
dedmihey <164859123+dedmihey@users.noreply.github.com>
2024-04-01 11:47:42 +0300

    Add files via upload

---
+++ homeworks5.py
@@ -0,0 +1,14 @@
+my_list = ['apfel', 'pflaume', 'birne', 'nuss', 'milone', 'wassermilone']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+my_list_ = my_list[2:5]
+print(my_list_)
+my_list[2] = 'orange'
+print(my_list)
+my_dict = {'apfel':'яблоко', 'pflaume':'слива', 'birne':'груша', 'orange':'апельсин'}
+print(my_dict)
+print(my_dict['apfel'])
+my_dict['nuss'] = 'орех'
+print(my_dict)
+
