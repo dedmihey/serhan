33c8a163478f7b3f4607d627f44f91616b806e54
dedmihey <164859123+dedmihey@users.noreply.github.com>
2024-04-10 00:40:05 +0300

    Add files via upload

--- homeworks11.py
+++ homeworks11.py
@@ -1,10 +1,12 @@
-def test (a, b):
+def test ():
+    a = 'bis'
+    b = 'bald'
     print(a, b)
 
 def test2(c, d, e):
     print(c, d, e)
 
-test('bis', 'bald')
+test()
 test2('Ich', 'bin', 'hier')
 
 
